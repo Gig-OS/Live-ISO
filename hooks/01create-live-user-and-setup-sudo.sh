e373371c97248e9662c946e769ad4dad90a689c8
Xin Yang <yangmame@icloud.com>
2024-05-16 09:45:11 +0800

    hooks: fix set user password

--- hooks/01create-live-user-and-setup-sudo.sh
+++ hooks/01create-live-user-and-setup-sudo.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # add live user with 'live' as password
-crun useradd -m live -p '$6$9PnXcWXTMZPm2w/y$vLDpBiW5PLHPD.pC4wN8N5fwNJJ9Q1xWmZwTb0tPLvUYHkUl8bmb5Yxn0HRP9F.GIuWk7o3SsEewyXhsW9EL.1'
+crun useradd -m live 
+crun 'echo -e "live\nlive" | passwd live'
+crun 'echo -e "live\nlive" | passwd'
 
 # add to wheel to use sudo
 crun gpasswd -a live wheel
 
-crun sed -i 's/.*%wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/g' /etc/sudoers
+sed -i 's/.*%wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/g' "${WORKDIR}/squashfs/etc/sudoers"
