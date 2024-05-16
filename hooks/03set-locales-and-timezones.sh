8b8aae537339d34cb2ac1fcb8ebdcef7a63e8f67
Xin Yang <yangmame@icloud.com>
2024-05-16 11:43:20 +0800

    hooks: check locales if has been set

--- hooks/03set-locales-and-timezones.sh
+++ hooks/03set-locales-and-timezones.sh
@@ -3,6 +3,8 @@
 echo "Asia/Shanghai" > ${WORKDIR}/squashfs/etc/timezone
 crun emerge --config sys-libs/timezone-data
 
-echo -e "en_US.UTF-8 UTF-8\nzh_CN.UTF-8 UTF-8" >> ${WORKDIR}/squashfs/etc/locale.gen
-crun locale-gen
-crun eselect locale set zh_CN.utf8
+if ( ! grep -q ^zh_CN.UTF-8 ${WORKDIR}/squashfs/etc/locale.gen );then
+    echo -e "en_US.UTF-8 UTF-8\nzh_CN.UTF-8 UTF-8" >> ${WORKDIR}/squashfs/etc/locale.gen
+    crun locale-gen
+    crun eselect locale set zh_CN.utf8
+fi
